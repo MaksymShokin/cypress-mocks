989f6df725819e0067cecef5f3a1a5937f038616
Maksym Shokin <problaze7@gmail.com>
2023-04-06 21:14:39 +0300

    using-clock

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -2,6 +2,7 @@
 
 describe('share location', () => {
   beforeEach(() => {
+    cy.clock();
     cy.fixture('user-location.json').as('userLocation');
     cy.visit('/').then(window => {
       cy.get('@userLocation').then(fakePosition => {
@@ -53,5 +54,10 @@ describe('share location', () => {
     });
 
     cy.get('@getItem').should('have.been.called');
+
+    cy.get('[data-cy="info-message"]').should('be.visible');
+    cy.get('[data-cy="info-message"]').should('have.class', 'visible');
+    cy.tick(2000);
+    cy.get('[data-cy="info-message"]').should('not.be.visible');
   });
 });
